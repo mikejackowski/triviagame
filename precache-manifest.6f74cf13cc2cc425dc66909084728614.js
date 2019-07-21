c34e729a82756a80bc99a8a7c66937d02bfbf691
Clue Less <dotamr0@gmail.com>
2019-07-21 13:24:29 +0200

    Updates

---
+++ precache-manifest.6f74cf13cc2cc425dc66909084728614.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e718e2bde2fb6505b28c016a920360f6",
+    "url": "/triviagame/index.html"
+  },
+  {
+    "revision": "486ef692c9753480c867",
+    "url": "/triviagame/static/css/main.b7102639.chunk.css"
+  },
+  {
+    "revision": "12f7adcf50121a6684a8",
+    "url": "/triviagame/static/js/2.75501045.chunk.js"
+  },
+  {
+    "revision": "486ef692c9753480c867",
+    "url": "/triviagame/static/js/main.98215d73.chunk.js"
+  },
+  {
+    "revision": "8a004959c87682ae6d3b",
+    "url": "/triviagame/static/js/runtime~main.eb595a49.js"
+  }
+]);
